7fe71e8e1ea5ce02944b970a74acfedd04a9f612
almudza <alifulmudzakir@gmail.com>
2020-02-05 20:50:33 -0500

    stup dev-server with index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Movie List");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Movie dList</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
